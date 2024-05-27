03146cfd71ff7d02b5f65187fa436434f5f4314e
rizkyrivaldi <rizkyrivaldi00@gmail.com>
2024-05-27 13:46:46 +0700

    last S movement patch

--- src/direct_control/direct_control/position_custom_S.py
+++ src/direct_control/direct_control/position_custom_S.py
@@ -31,14 +31,23 @@ class OffboardControl(Node):
         # Init position setpoints
         self.current_mission = 0
         self.takeoff_height = -5.0
-        self.position_setpoint = {
-            
-            "takeoff" : [0.0, 0.0, self.takeoff_height, 0.0],
-            "point_a" : [10.0, 0.0, self.takeoff_height, 0.0],
-            "point_b" : [10.0, -10.0, self.takeoff_height, -90.0],
-            "point_c" : [0.0, -10.0, self.takeoff_height, -180.0],
-            "point_d" : [0.0, -20.0, self.takeoff_height, -90.0],
-            "point_e" : [10.0, -20.0, self.takeoff_height, 0.0]
+        self.position_setpoint = {        
+
+            # With Yaw Rotation
+            # "takeoff" : [0.0, 0.0, self.takeoff_height, 0.0],
+            # "point_a" : [10.0, 0.0, self.takeoff_height, 0.0],
+            # "point_b" : [10.0, -10.0, self.takeoff_height, -90.0],
+            # "point_c" : [0.0, -10.0, self.takeoff_height, -180.0],
+            # "point_d" : [0.0, -20.0, self.takeoff_height, -90.0],
+            # "point_e" : [10.0, -20.0, self.takeoff_height, 0.0]
+
+            # Without Yaw Rotation
+            "takeoff" : [0.0, 0.0, self.takeoff_height, 90.0],
+            "point_a" : [10.0, 0.0, self.takeoff_height, 90.0],
+            "point_b" : [10.0, -10.0, self.takeoff_height, 90.0],
+            "point_c" : [0.0, -10.0, self.takeoff_height, 90.0],
+            "point_d" : [0.0, -20.0, self.takeoff_height, 90.0],
+            "point_e" : [10.0, -20.0, self.takeoff_height, 90.0]
         }
 
         # RUN THE LOOP
